4a4ccc38befe8e946c5bc6bbe682dfd65a81a562
Tevi Placca <teviplacca@gmail.com>
2024-12-27 16:03:16 -0500

    modifify assignment.js file

--- js/assignment.js
+++ js/assignment.js
@@ -3,15 +3,22 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
+const responseEl = document.getElementById("response");// Immutable variable
 
-// create a mutable variable called age and do not assign it a value.
-
+// create a mutable variable called age and do not assign it a value
+let age
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
-    // Write your code below
-
+    // Write your conditional logic inside this function
+    if (age >= 18) { 
+       responseEl.innerText = "You are an adult."; 
+    } else if (age > 0) { 
+       responseEl.innerText = "You are a minor.";
+    } else { 
+       responseEl.innerText = "Please enter a valid age.";
+    }
 }
 
 
